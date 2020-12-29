ed13ed4dc5b48974e6895e5238c741d68b561a7c
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2020-12-29 22:40:55 +0100

    SVG
    Svg eingebunden
    Fehlende Backround svg

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,26 +1,45 @@
 <template>
   <div>
-    <h1>ReUse und Secondhand</h1>
-    <p class="hero-subline">in Deutschland</p>
-    <p class="large text-center">Einstellungen zum Thema Abfallvermeidung und Nachhaltigkeit</p>
-    <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021
-      von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
 
-    <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
-    <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
-    <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
+    <div class="container">
+      <h1>ReUse und Secondhand</h1>
+      <p class="hero-subline">in Deutschland</p>
+      <p class="large text-center">Einstellungen zum Thema Abfallvermeidung und Nachhaltigkeit</p>
+      <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021
+        von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
 
+      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
+      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
+      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
+
+      <div class="row-full">
+        <img src="../assets/people.svg" alt="Menge von Leuten">
+      </div>
+    </div>
+
+<div class="container">
+  <div class="row">
     <p class="topline topline-green">Wie stehen Deutsche zu Klimaschutz und Nachhaltigkeit?</p>
+  </div>
     <h2>Ziel der Studie</h2>
     <p>In Deutschland verursacht jeder Bewohner durchschnittlich 535 kg Abfälle pro Jahr. Dazu kommen Abfälle, die bei
       der Produktion dieser Artikel anfallen. Insgesamt beläuft sich das Pro-Kopf-Abfallaufkommen der Deutschen auf 2748
       kg jährlich.
       Die zugrunde liegende Studie versucht, die Beweggründe für den Umgang mit Secondhand-Produkten zu verstehen,
       indem sie die allgemeine Einstellung zu den Themen Klimaschutz und Nachhaltigkeit abfragt. </p>
+<div class="background_green"></div>
 
+    <img src="../assets/trash.svg" alt="Hintergrundfläche grün">
     <h2 class="rubbish">535 kg</h2>
     <p class="rubbish">Pro-Kopf-Abfallaufkommen</p>
 
+
+</div>
+
+<img src="../assets/herausgeber.svg" alt="Herausgeber">
+<img src="../assets/befragte.svg" alt="Befragte">
+
+<div class="background_red">
     <p class="topline topline-white text-center">Wer hat die Stuide durchgeführt?</p>
     <h2 class="text-center">Die Studie</h2>
     <p class="large">Herausgeber</p>
@@ -33,7 +52,11 @@
       voll und ganz zustimmen oder eher zustimmen.</p>
     <p>An der Studie nahmen insgesamt 1.023 Personen aus Deutschland teil, sodass die Stichprobe in Hinblick auf Alter,
       Geschlecht, Region und Konsumverhalten hinreichend unterscheiden werden kann. </p>
+</div>
 
+
+
+<!--Lena-->
     <p class="topline topline-green">Gruppierte Vergleiche </p>
     <h2 class="text-center">Zielgruppen</h2>
     <p class="text-center large">Der Grad der Zustimmung dieser Aussagen wird in der folgenden Datenansicht in drei
