ea647b5488bc3a9f054614e4fe394a4898dc9130
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2020-12-31 17:59:09 +0100

    Section Studie
    background image eingebunden, text grob positioniert

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,7 +5,8 @@
       <div class="container-herotext">
         <p class="hero-subline">in Deutschland</p>
         <p class="p-large text-center">Einstellungen zum Thema Abfallvermeidung und Nachhaltigkeit</p>
-        <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021 von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
+        <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021
+          von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
       </div>
       <div class="container-speechbubbles">
         <img src="../assets/speechbubble-1.svg" alt="Menge von Leuten">
@@ -16,14 +17,18 @@
         <img src="../assets/people.svg" alt="Menge von Leuten">
       </div>
     </div>
+
     <div class="container-ziel-studie">
       <div class="container-text">
         <p class="topline topline-green">Wie stehen Deutsche zu Klimaschutz und Nachhaltigkeit?</p>
         <h2>Ziel der Studie</h2>
-        <p>In Deutschland verursacht jeder Bewohner durchschnittlich 535 kg Abfälle pro Jahr. Dazu kommen Abfälle, die bei
-          der Produktion dieser Artikel anfallen. Insgesamt beläuft sich das Pro-Kopf-Abfallaufkommen der Deutschen auf 2748
-          kg jährlich. Die zugrunde liegende Studie versucht, die Beweggründe für den Umgang mit Secondhand-Produkten zu verstehen,
-          indem sie die allgemeine Einstellung zu den Themen Klimaschutz und Nachhaltigkeit abfragt. 
+        <p>In Deutschland verursacht jeder Bewohner durchschnittlich 535 kg Abfälle pro Jahr. Dazu kommen Abfälle, die
+          bei
+          der Produktion dieser Artikel anfallen. Insgesamt beläuft sich das Pro-Kopf-Abfallaufkommen der Deutschen auf
+          2748
+          kg jährlich. Die zugrunde liegende Studie versucht, die Beweggründe für den Umgang mit Secondhand-Produkten zu
+          verstehen,
+          indem sie die allgemeine Einstellung zu den Themen Klimaschutz und Nachhaltigkeit abfragt.
         </p>
       </div>
       <div class="container-rubbish">
@@ -31,28 +36,47 @@
         <img src="../assets/background-green.png" alt="Hintergrundfläche grün">
       </div>
     </div>
-    <div>
-    <img src="../assets/herausgeber.svg" alt="Herausgeber">
-    <img src="../assets/befragte.svg" alt="Befragte">
-    <div class="background_red">
+
+
+
+    <div class="container-stuide background_red">
+    <div class="row-illus">
+      <img src="../assets/herausgeber.svg" alt="Herausgeber">
+      <img src="../assets/befragte.svg" alt="Befragte">
+    </div>
+      <div>
         <p class="topline topline-white text-center">Wer hat die Stuide durchgeführt?</p>
         <h2 class="text-center">Die Studie</h2>
-        <p class="p-large">Herausgeber</p>
-        <p>Die dargestellten Inhalte sind Teil einer empirischen Studie aus dem Jahr 2020, welche vom Wuppertal Institut für
-          Klima, Umwelt und Energie durchgeführt wurde. Die Erhebung erfolgte in Zusammenarbeit mit dem
-          Befragungsunternehmen
-          YouGov und wurde von Ebay Kleinanzeigen finanziell unterstützt. </p>
-        <p class="p-large">Methodik und Teilnehmer</p>
-        <p>Die Daten der Studie wurden mittels einer Online-Befragung erhoben. Die Teilnehmer konnten den Aussagen entweder
-          voll und ganz zustimmen oder eher zustimmen.</p>
-        <p>An der Studie nahmen insgesamt 1.023 Personen aus Deutschland teil, sodass die Stichprobe in Hinblick auf Alter,
-          Geschlecht, Region und Konsumverhalten hinreichend unterscheiden werden kann. </p>
+
+        <div class="container-text-studie">
+          <div class="col">
+            <p class="p-large">Herausgeber</p>
+            <p>Die dargestellten Inhalte sind Teil einer empirischen Studie aus dem Jahr 2020, welche vom Wuppertal
+              Institut
+              für
+              Klima, Umwelt und Energie durchgeführt wurde. Die Erhebung erfolgte in Zusammenarbeit mit dem
+              Befragungsunternehmen
+              YouGov und wurde von Ebay Kleinanzeigen finanziell unterstützt. </p>
+          </div>
+          <div class="col">
+            <p class="p-large">Methodik und Teilnehmer</p>
+            <p>Die Daten der Studie wurden mittels einer Online-Befragung erhoben. Die Teilnehmer konnten den Aussagen
+              entweder
+              voll und ganz zustimmen oder eher zustimmen.</p>
+            <p>An der Studie nahmen insgesamt 1.023 Personen aus Deutschland teil, sodass die Stichprobe in Hinblick auf
+              Alter,
+              Geschlecht, Region und Konsumverhalten hinreichend unterscheiden werden kann. </p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
+
+
     <!--Lena-->
-    <p class="topline topline-green">Gruppierte Vergleiche </p>
+    <p class="topline topline-green text-center">Gruppierte Vergleiche </p>
     <h2 class="text-center">Zielgruppen</h2>
-    <p class="text-center p-large">Der Grad der Zustimmung dieser Aussagen wird in der folgenden Datenansicht in drei Kategorien betrachtet.</p>
+    <p class="text-center p-large">Der Grad der Zustimmung dieser Aussagen wird in der folgenden Datenansicht in drei
+      Kategorien betrachtet.</p>
 
     <div class="zielgruppe">
       <div class="card">
@@ -67,8 +91,9 @@
         <img src="../assets/karte.svg" alt="Karte">
         <div class="cardtext">
           <h3>Lokalisation</h3>
-          <p>Die Personen werden aufgrund ihres Wohnorts zusammengefasst, indem Personen aus dem Westen und Osten Deutschlands
-          jeweils eine Gruppe bilden.</p>
+          <p>Die Personen werden aufgrund ihres Wohnorts zusammengefasst, indem Personen aus dem Westen und Osten
+            Deutschlands
+            jeweils eine Gruppe bilden.</p>
           <p class="link">mehr erfahren</p>
         </div>
       </div>
@@ -77,11 +102,12 @@
         <img src="../assets/konsum.svg" alt="Konsumart">
         <div class="cardtext">
           <h3>Konsumart</h3>
-          <p>Die Teilnehmer der Studie sind gruppiert, je nachdem, ob sie Käufer:innen von gebrauchten Waren sind oder nicht.
-          Hier wird also hinsichtlich ihres Konsumverhaltens unterscheiden. </p>
+          <p>Die Teilnehmer der Studie sind gruppiert, je nachdem, ob sie Käufer:innen von gebrauchten Waren sind oder
+            nicht.
+            Hier wird also hinsichtlich ihres Konsumverhaltens unterscheiden. </p>
           <p class="link">mehr erfahren</p>
         </div>
-      </div>    
+      </div>
     </div>
     <p>zu den Daten</p>
   </div>
