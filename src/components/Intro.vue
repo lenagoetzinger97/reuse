8769982e0062a3c0b5717ba6aa54263984ec13c6
NWiesler <48588140+NWiesler@users.noreply.github.com>
2020-12-31 11:25:17 +0100

    Hero und Ziel der Studie hat spacing und ist gestylt

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,104 +1,90 @@
 <template>
   <div>
-
-    <div class="container">
+    <div>
       <h1>ReUse und Secondhand</h1>
-      <p class="hero-subline">in Deutschland</p>
-      <p class="large text-center">Einstellungen zum Thema Abfallvermeidung und Nachhaltigkeit</p>
-      <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021
-        von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
-
-      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
-      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
-      <p class="large text-center text-bubbles">Für die Umwelt schränke ich mich in meinem Konsum nicht ein.</p>
-
+      <div class="container-herotext">
+        <p class="hero-subline">in Deutschland</p>
+        <p class="p-large text-center">Einstellungen zum Thema Abfallvermeidung und Nachhaltigkeit</p>
+        <p class="text-center">Semesterarbeit im Kurs Interaktives Informationsdesign des Fachbereichs Onlinemedien 2021 von Nathalie Wiesler, Ronja Walter, Lena Götzinger, Lena Frerichs und Julia Fischinger </p>
+      </div>
+      <div class="container-speechbubbles">
+        <img src="../assets/speechbubble-1.svg" alt="Menge von Leuten">
+        <img src="../assets/speechbubble-2.svg" alt="Menge von Leuten">
+        <img src="../assets/speechbubble-3.svg" alt="Menge von Leuten">
+      </div>
       <div class="row-full">
         <img src="../assets/people.svg" alt="Menge von Leuten">
       </div>
     </div>
-
-<div class="container">
-  <div class="row">
-    <p class="topline topline-green">Wie stehen Deutsche zu Klimaschutz und Nachhaltigkeit?</p>
-  </div>
-    <h2>Ziel der Studie</h2>
-    <p>In Deutschland verursacht jeder Bewohner durchschnittlich 535 kg Abfälle pro Jahr. Dazu kommen Abfälle, die bei
-      der Produktion dieser Artikel anfallen. Insgesamt beläuft sich das Pro-Kopf-Abfallaufkommen der Deutschen auf 2748
-      kg jährlich.
-      Die zugrunde liegende Studie versucht, die Beweggründe für den Umgang mit Secondhand-Produkten zu verstehen,
-      indem sie die allgemeine Einstellung zu den Themen Klimaschutz und Nachhaltigkeit abfragt. </p>
-<div class="background_green"></div>
-
-    <img src="../assets/trash.svg" alt="Hintergrundfläche grün">
-    <h2 class="rubbish">535 kg</h2>
-    <p class="rubbish">Pro-Kopf-Abfallaufkommen</p>
-
-
-</div>
-
-<img src="../assets/herausgeber.svg" alt="Herausgeber">
-<img src="../assets/befragte.svg" alt="Befragte">
-
-<div class="background_red">
-    <p class="topline topline-white text-center">Wer hat die Stuide durchgeführt?</p>
-    <h2 class="text-center">Die Studie</h2>
-    <p class="large">Herausgeber</p>
-    <p>Die dargestellten Inhalte sind Teil einer empirischen Studie aus dem Jahr 2020, welche vom Wuppertal Institut für
-      Klima, Umwelt und Energie durchgeführt wurde. Die Erhebung erfolgte in Zusammenarbeit mit dem
-      Befragungsunternehmen
-      YouGov und wurde von Ebay Kleinanzeigen finanziell unterstützt. </p>
-    <p class="large">Methodik und Teilnehmer</p>
-    <p>Die Daten der Studie wurden mittels einer Online-Befragung erhoben. Die Teilnehmer konnten den Aussagen entweder
-      voll und ganz zustimmen oder eher zustimmen.</p>
-    <p>An der Studie nahmen insgesamt 1.023 Personen aus Deutschland teil, sodass die Stichprobe in Hinblick auf Alter,
-      Geschlecht, Region und Konsumverhalten hinreichend unterscheiden werden kann. </p>
-</div>
-
-
-
-<!--Lena-->
+    <div class="container-ziel-studie">
+      <div class="container-text">
+        <p class="topline topline-green">Wie stehen Deutsche zu Klimaschutz und Nachhaltigkeit?</p>
+        <h2>Ziel der Studie</h2>
+        <p>In Deutschland verursacht jeder Bewohner durchschnittlich 535 kg Abfälle pro Jahr. Dazu kommen Abfälle, die bei
+          der Produktion dieser Artikel anfallen. Insgesamt beläuft sich das Pro-Kopf-Abfallaufkommen der Deutschen auf 2748
+          kg jährlich. Die zugrunde liegende Studie versucht, die Beweggründe für den Umgang mit Secondhand-Produkten zu verstehen,
+          indem sie die allgemeine Einstellung zu den Themen Klimaschutz und Nachhaltigkeit abfragt. 
+        </p>
+      </div>
+      <div class="container-rubbish">
+        <img src="../assets/trash.svg" alt="Hintergrundfläche grün">
+        <img src="../assets/background-green.png" alt="Hintergrundfläche grün">
+      </div>
+    </div>
+    <div>
+    <img src="../assets/herausgeber.svg" alt="Herausgeber">
+    <img src="../assets/befragte.svg" alt="Befragte">
+    <div class="background_red">
+        <p class="topline topline-white text-center">Wer hat die Stuide durchgeführt?</p>
+        <h2 class="text-center">Die Studie</h2>
+        <p class="p-large">Herausgeber</p>
+        <p>Die dargestellten Inhalte sind Teil einer empirischen Studie aus dem Jahr 2020, welche vom Wuppertal Institut für
+          Klima, Umwelt und Energie durchgeführt wurde. Die Erhebung erfolgte in Zusammenarbeit mit dem
+          Befragungsunternehmen
+          YouGov und wurde von Ebay Kleinanzeigen finanziell unterstützt. </p>
+        <p class="p-large">Methodik und Teilnehmer</p>
+        <p>Die Daten der Studie wurden mittels einer Online-Befragung erhoben. Die Teilnehmer konnten den Aussagen entweder
+          voll und ganz zustimmen oder eher zustimmen.</p>
+        <p>An der Studie nahmen insgesamt 1.023 Personen aus Deutschland teil, sodass die Stichprobe in Hinblick auf Alter,
+          Geschlecht, Region und Konsumverhalten hinreichend unterscheiden werden kann. </p>
+    </div>
+    </div>
+    <!--Lena-->
     <p class="topline topline-green">Gruppierte Vergleiche </p>
     <h2 class="text-center">Zielgruppen</h2>
-    <p class="text-center large">Der Grad der Zustimmung dieser Aussagen wird in der folgenden Datenansicht in drei
-      Kategorien betrachtet. </p>
+    <p class="text-center p-large">Der Grad der Zustimmung dieser Aussagen wird in der folgenden Datenansicht in drei Kategorien betrachtet.</p>
 
-<div class="zielgruppe">
-
-  <div class="card">
-    <img src="../assets/zielgruppe.svg" alt="Zielgruppe">
-      <div class="cardtext">
-        <h3>Zielgruppen</h3>
-        <p>In einem Vergleich werden die unter 30 und über 30-jährigen Teilnehmer der Studie unterschieden.</p>
-        <p class="link">mehr erfahren</p>
+    <div class="zielgruppe">
+      <div class="card">
+        <img src="../assets/zielgruppe.svg" alt="Zielgruppe">
+        <div class="cardtext">
+          <h3>Zielgruppen</h3>
+          <p>In einem Vergleich werden die unter 30 und über 30-jährigen Teilnehmer der Studie unterschieden.</p>
+          <p class="link">mehr erfahren</p>
+        </div>
       </div>
-  </div>
-
-  <div class="card">
-    <img src="../assets/karte.svg" alt="Karte">
-      <div class="cardtext">
-        <h3>Lokalisation</h3>
-        <p>Die Personen werden aufgrund ihres Wohnorts zusammengefasst, indem Personen aus dem Westen und Osten Deutschlands
-        jeweils eine Gruppe bilden.</p>
-        <p class="link">mehr erfahren</p>
+      <div class="card">
+        <img src="../assets/karte.svg" alt="Karte">
+        <div class="cardtext">
+          <h3>Lokalisation</h3>
+          <p>Die Personen werden aufgrund ihres Wohnorts zusammengefasst, indem Personen aus dem Westen und Osten Deutschlands
+          jeweils eine Gruppe bilden.</p>
+          <p class="link">mehr erfahren</p>
+        </div>
       </div>
-  </div>
 
-  <div class="card">
-    <img src="../assets/konsum.svg" alt="Konsumart">
-      <div class="cardtext">
-        <h3>Konsumart</h3>
-        <p>Die Teilnehmer der Studie sind gruppiert, je nachdem, ob sie Käufer:innen von gebrauchten Waren sind oder nicht.
-        Hier wird also hinsichtlich ihres Konsumverhaltens unterscheiden. </p>
-        <p class="link">mehr erfahren</p>
-      </div>
-  </div>    
-</div>
-
-
+      <div class="card">
+        <img src="../assets/konsum.svg" alt="Konsumart">
+        <div class="cardtext">
+          <h3>Konsumart</h3>
+          <p>Die Teilnehmer der Studie sind gruppiert, je nachdem, ob sie Käufer:innen von gebrauchten Waren sind oder nicht.
+          Hier wird also hinsichtlich ihres Konsumverhaltens unterscheiden. </p>
+          <p class="link">mehr erfahren</p>
+        </div>
+      </div>    
+    </div>
     <p>zu den Daten</p>
-
-
-</div>
+  </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
